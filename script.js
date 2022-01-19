e90ead6bfad169f0af1db138a79b9ba8c7424c00
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-01-19 14:03:27 +0100

    new

--- script.js
+++ script.js
@@ -2,22 +2,17 @@ let video = document.getElementById("video");
 let audio = document.getElementById("audio");
 
 
-random = Math.floor(Math.random() * 4);
+random = Math.floor(Math.random() * 2);
 
 src1V = "./media/beach.mp4";
 src1A = "/media/beach.mp3";
 
-src2V = "./media/forest_V.mp4";
-src2A = "./media/forest_A.mp4";
+src2V = "./media/nightV.mp4";
+src2A = "./media/nightA.mp4";
 
-src3V = "./media/nightV.mp4";
-src3A = "./media/nightA.mp4";
 
-src4V = "./media/moreseaV.mp4";
-src4A = "./media/moreseaA.mp4";
-
-video_src = [src1V,src2V,src3V,src4V];
-audio_src = [src1A,src2A,src3A,src4A];
+video_src = [src1V,src2V];
+audio_src = [src1A,src2A];
 
 video.src = video_src[random];
 audio.src = audio_src[random];
