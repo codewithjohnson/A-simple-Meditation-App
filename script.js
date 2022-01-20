bf19f1b8db8dfc470769609e5c88f9df2f686d1c
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-01-20 15:47:48 +0100

    adjust

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 let video = document.getElementById("video");
 let audio = document.getElementById("audio");
 let disp = document.getElementById("med2");
+let btnPP = document.getElementById("btn-left");
+let stop = document.getElementById("btn-right");
 disp.play();
 
 
+let src1V = "./media/nightV.mp4";
+let src1A = "./media/nightA.mp4";
+let src2V = "./media/BFV.mp4";
+let src2A = "./media/BFA.mp4";
+
+let video_src = [src1V,src2V];
+let audio_src = [src1A,src2A];
+
+
 random = Math.floor(Math.random()*2);
-
-src1V = "./media/nightV.mp4";
-src1A = "./media/nightA.mp4";
-src2V = "./media/BFV.mp4";
-src2A = "./media/BFA.mp4";
-
-
-video_src = [src1V,src2V];
-audio_src = [src1A,src2A];
-
-
 video.src = video_src[random];
 audio.src = audio_src[random];
 
-let btnPP = document.getElementById("btn-left");
-let stop = document.getElementById("btn-right");
+
 
 let start = 0;
-
 function playpauseFunc(){
     if (start ===0){
         start = 1;
         video.play();
         audio.play();
-        // random(1,2);
         btnPP.innerHTML ="Pause &#9208;";
         disp.play();
     }
@@ -45,7 +42,6 @@ function playpauseFunc(){
 }
 
 function stopFunc(){
-    // playpauseFunc();
     video.pause();
     audio.pause();
     video.currentTime = 0;
@@ -53,3 +49,13 @@ function stopFunc(){
     disp.play();
 }
 
+
+function shuffle(){
+    console.log('it works');
+    rand = Math.floor(Math.random()*2);
+    video.src = video_src[rand];
+    audio.src = audio_src[rand];
+    video.play();
+    audio.play();
+
+}
--- t.js
+++
@@ -1,6 +0,0 @@
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
-
-x = random(1,4);
-console.log(x);
