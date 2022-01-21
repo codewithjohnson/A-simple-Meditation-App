4e99100f5cb2b3c16e210944255aa483feec78ef
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-01-21 17:00:45 +0100

    new

--- script.js
+++ script.js
@@ -12,12 +12,14 @@ let src2V = "./media/BFV.mp4";
 let src2A = "./media/BFA.mp4";
 let src3V = "./media/vooo.mp4";
 let src3A = "./media/piano.mp4";
+let src4V = "./media/vooo.mp4";
+let src4A = "./media/Project.mp4";
 
-let video_src = [src1V,src2V,src3V];
-let audio_src = [src1A,src2A,src3A];
+let video_src = [src1V,src2V,src3V,src3V];
+let audio_src = [src1A,src2A,src3A,];
 
 
-random = Math.floor(Math.random()*3);
+random = Math.floor(Math.random()*4);
 video.src = video_src[random];
 audio.src = audio_src[random];
 
@@ -54,7 +56,7 @@ function stopFunc(){
 
 function shuffle(){
    
-    rand = Math.floor(Math.random()*3);
+    rand = Math.floor(Math.random()*4);
     video.src = video_src[rand];
     audio.src = audio_src[rand];
     video.play();
